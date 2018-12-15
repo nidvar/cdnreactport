740fa372033b42d89ff8d11e8217f44258879460
Divine Potato <djayshine87@hotmail.com>
2018-12-15 15:07:44 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
 if(document.URL.indexOf("https://nidvar.github.io/index.html") >= 0){
 	document.querySelector('.homeb').classList.add('blink');
-        document.querySelector('.aboutb').classList.remove('blink');
-        document.querySelector('.workb').classList.remove('blink');
-        document.querySelector('.contactb').classList.remove('blink');
 	}
-    if(document.URL.indexOf("about.html") >= 0){
-        document.querySelector('.homeb').classList.remove('blink');
-        document.querySelector('.aboutb').classList.add('blink');
-        document.querySelector('.workb').classList.remove('blink');
-        document.querySelector('.contactb').classList.remove('blink');
-    }
-    if(document.URL.indexOf("work.html") >= 0){
-        document.querySelector('.homeb').classList.remove('blink');
-        document.querySelector('.aboutb').classList.remove('blink');
-        document.querySelector('.workb').classList.add('blink');
-        document.querySelector('.contactb').classList.remove('blink');
-    }
-    if(document.URL.indexOf("contact.html") >= 0){
-        document.querySelector('.homeb').classList.remove('blink');
-        document.querySelector('.aboutb').classList.remove('blink');
-        document.querySelector('.workb').classList.remove('blink');
-        document.querySelector('.contactb').classList.add('blink');
-    }
