734a5b7f020a4956aa7934397a73a4583e4c04d9
Divine Potato <djayshine87@hotmail.com>
2018-12-15 15:11:02 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 if(document.URL.indexOf("https://nidvar.github.io/index.html") >= 0){
 	document.querySelector('.homeb').classList.add('blink');
+	document.querySelector('.aboutb').classList.remove('blink');
+	}
+if(document.URL.indexOf("https://nidvar.github.io/about.html") >= 0){
+	document.querySelector('.homeb').classList.remove('blink');
+	document.querySelector('.aboutb').classList.add('blink');
 	}
