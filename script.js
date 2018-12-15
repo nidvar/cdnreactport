b32b6516ff99d2ac6dac94cd01562b7b5b2a4f30
Divine Potato <djayshine87@hotmail.com>
2018-12-15 15:15:33 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+if(document.URL.indexOf("index.html") >= 0){
+	document.querySelector('.homeb').classList.add('blink');
+	document.querySelector('.aboutb').classList.remove('blink');
+	alert('hi');
+	}
+if(document.URL.indexOf("about.html") >= 0){
+	document.querySelector('.homeb').classList.remove('blink');
+	document.querySelector('.aboutb').classList.add('blink');
+	}
