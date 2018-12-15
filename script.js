9ae85a6eaeeb80e2fc5004330f55232ece6ba85f
Divine Potato <djayshine87@hotmail.com>
2018-12-15 14:21:41 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 if(document.URL.indexOf("index.html") >= 0){
-	    	document.querySelector('.home').classList.add('blink');
-        document.querySelector('.about').classList.remove('blink');
-        document.querySelector('.work').classList.remove('blink');
-        document.querySelector('.contact').classList.remove('blink');
+	    	document.querySelector('.homeb').classList.add('blink');
+        document.querySelector('.aboutb').classList.remove('blink');
+        document.querySelector('.workb').classList.remove('blink');
+        document.querySelector('.contactb').classList.remove('blink');
 	}
     if(document.URL.indexOf("about.html") >= 0){
-        document.querySelector('.home').classList.remove('blink');
-        document.querySelector('.about').classList.add('blink');
-        document.querySelector('.work').classList.remove('blink');
-        document.querySelector('.contact').classList.remove('blink');
+        document.querySelector('.homeb').classList.remove('blink');
+        document.querySelector('.aboutb').classList.add('blink');
+        document.querySelector('.workb').classList.remove('blink');
+        document.querySelector('.contactb').classList.remove('blink');
     }
     if(document.URL.indexOf("work.html") >= 0){
-        document.querySelector('.home').classList.remove('blink');
-        document.querySelector('.about').classList.remove('blink');
-        document.querySelector('.work').classList.add('blink');
-        document.querySelector('.contact').classList.remove('blink');
+        document.querySelector('.homeb').classList.remove('blink');
+        document.querySelector('.aboutb').classList.remove('blink');
+        document.querySelector('.workb').classList.add('blink');
+        document.querySelector('.contactb').classList.remove('blink');
     }
     if(document.URL.indexOf("contact.html") >= 0){
-        document.querySelector('.home').classList.remove('blink');
-        document.querySelector('.about').classList.remove('blink');
-        document.querySelector('.work').classList.remove('blink');
-        document.querySelector('.contact').classList.add('blink');
+        document.querySelector('.homeb').classList.remove('blink');
+        document.querySelector('.aboutb').classList.remove('blink');
+        document.querySelector('.workb').classList.remove('blink');
+        document.querySelector('.contactb').classList.add('blink');
     }
