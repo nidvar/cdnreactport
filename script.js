4e82ec42f243612dfcbe9b0a0b95902993ec44ea
Divine Potato <djayshine87@hotmail.com>
2018-12-26 15:52:12 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-if(document.URL.indexOf("index.html") >= 0){
-	document.querySelector('.homeb').classList.add('blink');
-	document.querySelector('.aboutb').classList.remove('blink');
-	alert('hi');
-	}
-if(document.URL.indexOf("about.html") >= 0){
-	document.querySelector('.homeb').classList.remove('blink');
-	document.querySelector('.aboutb').classList.add('blink');
-	}
+var homeNav = document.getElementById('home');
+var aboutNav = document.getElementById('about');
+var portfolioNav = document.getElementById('portfolio');
+var contactNav = document.getElementById('contact');
+
+
+
+var homeSection = document.querySelector('.homeSection');
+var aboutSection = document.querySelector('.aboutSection');
+var portfolioSection = document.querySelector('.portfolioSection');
+var contactSection = document.querySelector('.contactSection');
+
+function clickAbout(){
+	aboutSection.classList.remove('hide');
+	homeSection.classList.add('hide');
+	portfolioSection.classList.add('hide');
+	contactSection.classList.add('hide');
+}
+function clickHome(){
+	aboutSection.classList.add('hide');
+	homeSection.classList.remove('hide');
+	portfolioSection.classList.add('hide');
+	contactSection.classList.add('hide');
+}
+function clickPortfolio(){
+	aboutSection.classList.add('hide');
+	homeSection.classList.add('hide');
+	portfolioSection.classList.remove('hide');
+	contactSection.classList.add('hide');
+}
+function clickContact(){
+	aboutSection.classList.add('hide');
+	homeSection.classList.add('hide');
+	portfolioSection.classList.add('hide');
+	contactSection.classList.remove('hide');
+}
+
+aboutNav.addEventListener('click', clickAbout);
+homeNav.addEventListener('click', clickHome);
+contactNav.addEventListener('click', clickContact);
+portfolioNav.addEventListener('click', clickPortfolio);
