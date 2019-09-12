3502a3bfe06e4b2f518400cbf5d52bc2b8d103ea
nidvar <djayshine87@hotmail.com>
2019-09-12 19:42:08 +1000

    scrolling fixed navbar

--- Nav.jsx
+++ Nav.jsx
@@ -1,12 +1,29 @@
 class Nav extends React.Component{
+    state={
+        one:'ulcontainer'
+    }
+    handleScroll=()=>{
+        window.onscroll=()=>{
+            if (window.scrollY==0) {
+                this.setState({
+                    one:'ulcontainer'
+                },()=>{console.log(this.state)})
+              }else{
+                this.setState({
+                    one:'ulcontainer sticky'
+                },()=>{console.log(this.state)})
+              }
+        } 
+    }
     render(){
         return(
             <div>
+                {this.handleScroll()}
                 <div className='nav'>
-                    <ul className='ulcontainer'>
+                    <ul className={this.state.one}>
                         <li><a href='#'>APPS</a></li>
                         <li><a href='#'>WEBSITES</a></li>
-                        <li><a href='#'>RESUME</a></li>
+                        <li><a href='#'>ABOUT</a></li>
                     </ul>
                 </div>
             </div>
