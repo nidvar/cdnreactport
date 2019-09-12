716a4c04292ea710d50183967e75146e327c252f
nidvar <djayshine87@hotmail.com>
2019-09-12 18:40:36 +1000

    created footer

--- Footer.jsx
+++ Footer.jsx
@@ -4,9 +4,13 @@ class Footer extends React.Component{
             <div className='footer'>
                 {/* start here */}
 
-                
-                <h1>FOOTER</h1>
-
+                <div id='footer-container'>
+                    <div className='iconbox'><a href='https://www.facebook.com/'><img src='images/fbicon.png'/></a></div>
+                    <div className='iconbox'><a href='https://www.instagram.com/'><img src='images/ticon.png'/></a></div>
+                    <div className='iconbox'><a href='https://twitter.com/?lang=en'><img src='images/igicon.png'/></a></div>
+                </div>       
+                <br/>
+                <p className='copyright'>© Copyright 2019 | <a href='https://nidvar.github.io/'>By Nidvar</a></p>
 
                 {/* end here */}
             </div>
