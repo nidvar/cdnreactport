9246f103b40b38dcf9f5f9ac10bc31c8f516a3d6
nidvar <djayshine87@hotmail.com>
2019-09-13 18:14:10 +1000

    parallax added

--- Display.jsx
+++ Display.jsx
@@ -9,22 +9,27 @@ class Display extends React.Component{
 
 
                         
-                    <Statement text='App Portfolio'/>
+                    <Statement text='Major Projects'/>
                     <div className='body'>
-                        <div className='appdiv'><a href='https://nidvar.github.io/weather/'><Appbox image='images/weather.png' text='Weather App' /></a></div>
-                        <div className='appdiv'><a href='https://nidvar.github.io/landing/'><Appbox image='images/todo_list.png' text='Todo List'/></a></div>
-                        <div className='appdiv'><Appbox image='images/games.png' text='Games App' /></div>
-                        <div className='appdiv'><a href='https://nidvar.github.io/typingtest/'><Appbox image='images/typingtest.png' text='Typing Test'/></a></div>
+                        <div className='appdiv major'><a href='#'><Appbox image='images/weather.png' text='E-Commerce' /></a></div>
+                        <div className='appdiv major'><a href='https://nidvar.github.io/typingtest/'><Appbox image='images/typingtest.png' text='Color Project'/></a></div>
                     </div>
                     
 
                     <Parallax style_prop='parallax1' />
 
+                    <Statement text='Apps'/>
+                    <div className='body'>
+                        <div className='appdiv'><a href='https://nidvar.github.io/weather/'><Appbox image='images/weather.png' text='Weather App' /></a></div>
+                        <div className='appdiv'><a href='https://nidvar.github.io/landing/'><Appbox image='images/todo_list.png' text='Todo List'/></a></div>
+                        <div className='appdiv'><a href='https://nidvar.github.io/landing/'><Appbox image='images/games.png' text='Mini Games' /></a></div>
+                        <div className='appdiv'><a href='https://nidvar.github.io/typingtest/'><Appbox image='images/typingtest.png' text='Typing Test'/></a></div>
+                    </div>
+
                     <Statement text='Live Websites'/>
 
                     <div className='bodyweb'>
                         <div className='appdiv'><a href='http://goldenfalafel.com.au/'><Appbox image='images/goldenfalafel.png' /></a></div>
-                        <div className='appdiv'><a href='https://www.db-homes.com.au/'><Appbox image='images/dbhomes.png' /></a></div>
                         <div className='appdiv'><a href='https://www.meadanhomes.com.au/'><Appbox image='images/meadanhomes.png' /></a></div>
                         <div className='blackback appdiv'><a href='http://u4cmartialarts.com.au/'><Appbox image='images/u4c.png' /></a></div>
                         <div className='appdiv'><a href='https://thetaxfactor.com.au/'><Appbox image='images/taxfactor.png' /></a></div>
