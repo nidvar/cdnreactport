9143ca5b18d8d7aed053ab3ddb3f5234210bd317
Nidvar <djayshine87@hotmail.com>
2019-09-14 16:17:37 +1000

    Update Display.jsx

--- Display.jsx
+++ Display.jsx
@@ -19,7 +19,7 @@ class Display extends React.Component{
                     <div className='body'>
                         <div className='appdiv'><a href='https://nidvar.github.io/weather/'><Appbox image='images/weather.png' text='Weather App' /></a></div>
                         <div className='appdiv'><a href='https://nidvar.github.io/landing/'><Appbox image='images/todo_list.png' text='Todo List'/></a></div>
-                        <div className='appdiv'><a href='https://nidvar.github.io/landing/'><Appbox image='images/games.png' text='Mini Games' /></a></div>
+                        <div className='appdiv'><a href='https://nidvar.github.io/games/'><Appbox image='images/games.png' text='Mini Games' /></a></div>
                         <div className='appdiv'><a href='https://nidvar.github.io/typingtest/'><Appbox image='images/typingtest.png' text='Typing Test'/></a></div>
                     </div>
 
