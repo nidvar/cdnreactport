7150198ac69272777f0eec3bd106247367860e5c
nidvar <djayshine87@hotmail.com>
2019-09-20 20:49:23 +1000

    apps page

--- About.jsx
+++ About.jsx
@@ -2,11 +2,12 @@ class About extends React.Component{
     render(){
         return(
             <div className='About'>
-                <h3>Profile</h3>
-                <p>I graduated university with a bachelor degree in Science, majoring in Physics and Nanotechnology. I dabbled with HTML/CSS/PHP as a hobbie then focused heavily on JavaScript. Now I identify as a JavaScript developer working with React and Node.</p>
-
-                <h3>Current Status</h3>
-                <p>.....</p>
+                <h3>Background</h3>
+                <p>University graduate in Science, majoring in Physics and Nanotechnology. Dabbled with HTML/CSS/PHP as a hobbie then focused heavily on JavaScript. Now a JavaScript developer working with React and Node.</p>
+                <br />
+                <h3>My Apps</h3>
+                <p>Confident building apps with vanilla JS, followed by ReactJS. Experienced with Node JS, but main focus has been client side.</p>
+                <p>Most ReactJS apps on this portfolio use React/Babel CDN, with a few exceptions using create-react-app. Apps that are purely JS with no libraries use Webpack and Babel.</p>
             </div>
         )
     }
--- Apps.jsx
+++ Apps.jsx
@@ -1,8 +1,20 @@
 class Apps extends React.Component{
     render(){
         return(
-            <div>
-                <h1>Apps</h1>
+            <div className='Apps'>
+                <div className='Major'><h3>Major Projects</h3></div>
+                
+                <div className='Justapps'>
+                    <h3>Apps</h3>
+                    <div className='Justappsflex'>
+                        <AppBoxes className='appboxes' title='Online Chatroom' image='images/chatroom.png' source='https://nidvar.github.io/chatbox/' />
+                        <AppBoxes className='appboxes' title='Notes' image='images/todo_list.png' source='https://nidvar.github.io/landing/' />
+                        <AppBoxes className='appboxes' title='Games' image='images/games.png' source='https://nidvar.github.io/games/' />
+                        <AppBoxes className='appboxes' title='Weather App' image='images/weather.png' source='https://nidvar.github.io/weather/' />
+                        <AppBoxes className='appboxes' title='Find Images' image='images/search.png' source='https://nidvar.github.io/searchimage/' />
+                        <AppBoxes className='appboxes' title='Typing Test' image='images/typingtest.png' source='https://nidvar.github.io/typingtest/' />
+                    </div>
+                </div>   
             </div>
         )
     }
