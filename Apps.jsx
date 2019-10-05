3472929fcad0a935bc8e315fd386cfc3dbd8ca78
Nidvar <djayshine87@hotmail.com>
2019-10-05 10:33:04 +1000

    Update Apps.jsx

--- Apps.jsx
+++ Apps.jsx
@@ -10,7 +10,7 @@ class Apps extends React.Component{
                 <div className='Justapps'>
                     <h3>Apps</h3>
                     <div className='Justappsflex'>
-                        <AppBoxes className='appboxes' title='Online Chatroom' image='images/chatroom.png' source='https://nidvar.github.io/chatbox/' />
+                        <AppBoxes className='appboxes' title='Online Chatroom' image='images/chatroom.png' source='https://nidvar.github.io/chatroom/' />
                         <AppBoxes className='appboxes' title='Notes' image='images/todo_list.png' source='https://nidvar.github.io/landing/' />
                         <AppBoxes className='appboxes' title='Games' image='images/games.png' source='https://nidvar.github.io/games/' />
                         <AppBoxes className='appboxes' title='Weather App' image='images/weather.png' source='https://nidvar.github.io/weather/' />
